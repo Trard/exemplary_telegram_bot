feeb067dcf182049610cb804abbdeeda45c502f9
Trard <rivaavir3@gmail.com>
2021-07-12 18:33:13 +0300

    code style

--- src/index.js
+++ src/index.js
@@ -3,26 +3,32 @@ const winston = require('winston');
 require('dotenv').config();
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error'
+      }),
+        new winston.transports.File({
+            filename: 'logs/combined.log'
+    }),
   ],
 });
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+//logger middleware
 bot.use((ctx) => {
-    logger.info(ctx.update)
-})
+    logger.info(ctx.update);
+});
 
 bot.command('hi', (ctx) => {
-    ctx.reply(`Hello!`)
-})
+    ctx.reply(`Hello!`);
+});
 
-bot.launch()
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
